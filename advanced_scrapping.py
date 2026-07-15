f70f7c3db1300aa5ab8dd0d2f53a227a71b34780
sucupirap <pedro41julio@gmail.com>
2026-07-15 20:34:41 -0300

    adicionando link do autor as citacoes de frases

---
+++ advanced_scrapping.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+URL = "https://quotes.toscrape.com/"
+
+def obter_html(url):
+
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()  
+    return response
+
+def extrair_citacoes(html):
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    citacoes = []
+
+    for quote in soup.find_all('div', class_='quote'):
+        
+        texto = quote.find('span', class_='text').get_text(strip=True)
+        autor = quote.find('small', class_='author').get_text(strip=True)
+
+        tags = [tag.get_text(strip=True) for tag in quote.find_all('a', class_='tag')]
+        link_autor = quote.find('a')['href']
+
+        citacoes.append({'texto': texto, 'autor': autor, 'tags': tags, 'link_autor': urljoin(URL, link_autor)})
+
+    return citacoes
+
+def main():
+
+    html = obter_html(URL).text
+    citacoes = extrair_citacoes(html)
+
+    for c in citacoes:
+        print(f"{c['texto']} - {c['autor']} - Tags: {', '.join(c['tags'])} - Link do autor: {c['link_autor']}")
+
+if __name__ == "__main__":
+    main()
